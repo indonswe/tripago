4250b7c2e654dcc17a97db42507e80844c4245fd
indonswe <afc2002afc@hotmail.com>
2021-11-07 18:36:39 +0100

    changed the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
 
-import './App.css';
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  const [showEvents, setShowEvents] = useState(true)
+  const [events, setEvents] = useState([
+    {title: "mario's birthday bash", id: 1},
+    {title: "bowser's live stream", id: 2},
+    {title: "race on moo moo farm", id: 3}
+  ])
+
+  console.log(showEvents)
+
+  const handleClick = (id) => {
+    setEvents(prevEvents => {
+      return prevEvents.filter(event => id !== event.id)
+    })
+  }
+
+  return (
+    <div className="App">
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>Hide Events</button>
+        </div>
+      )}
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>Show Events</button>
+        </div>
+      )}  
+      {showEvents && events.map((event, index) => (
+        <div key={event.id}>
+          <h2>{index} - {event.title}</h2>
+          <button onClick={() => handleClick(event.id)}>delete event</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
+
+/*import './App.css';
 
 function App() {
   return (
@@ -9,4 +52,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
